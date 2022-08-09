d37580dddd98b00e3822cad1247ca4f60290b5f7
Hellrison Soares <64223482+hellrison@users.noreply.github.com>
2022-08-09 17:11:27 -0300

    READEME
    atualização do README

--- main.java
+++ main.java
@@ -6,7 +6,9 @@ public class main {
         g1.addEdge(1, 0, 3);
         g1.addEdge(0, 3, 4);
         g1.addEdge(3, 0, 4);
+        g1.addEdge(3, 2, 4);
         g1.addEdge(3, 4, 2); // AVISO
+
         System.out.println(g1);
         g1.degree(0);
         g1.degree(1);
