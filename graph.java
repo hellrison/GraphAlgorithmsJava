fff427ae15ceaa2ebe463e25739de35244ce4b64
Hellrison Soares <64223482+hellrison@users.noreply.github.com>
2022-08-29 22:15:53 -0300

    criação de grafo nao orientado e outros

--- graph.java
+++ graph.java
@@ -1,107 +1,182 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class graph {
-    private int countNodes;
-    private int countEdges;
-    private int[][] adjMatrix;
 
-    public graph(int countNodes) {
-        this.countNodes = countNodes;
-        this.adjMatrix = new int[countNodes][countNodes];
+  private int countNodes;
+  private int countEdges;
+  private int[][] adjMatrix;
+
+  public graph(int countNodes) {
+    this.countNodes = countNodes;
+    this.adjMatrix = new int[countNodes][countNodes];
+  }
+
+  public graph(String fileName) throws IOException {
+    File file = new File(fileName);
+    FileReader reader = new FileReader(file);
+    BufferedReader bufferedReader = new BufferedReader(reader);
+
+    // Read header
+    String[] line = bufferedReader.readLine().split(" ");
+    this.countNodes = (Integer.parseInt(line[0]));
+    int fileLines = (Integer.parseInt(line[1]));
+
+    // Create and fill adjMatrix with read edges
+    this.adjMatrix = new int[this.countNodes][this.countNodes];
+    for (int i = 0; i < fileLines; ++i) {
+      String[] edgeInfo = bufferedReader.readLine().split(" ");
+      int source = Integer.parseInt(edgeInfo[0]);
+      int sink = Integer.parseInt(edgeInfo[1]);
+      int weight = Integer.parseInt(edgeInfo[2]);
+      addEdge(source, sink, weight);
     }
+    bufferedReader.close();
+    reader.close();
+  }
 
-    public int getCountNodes() {
-        return countNodes;
+  public int getCountNodes() {
+    return this.countNodes;
+  }
+
+  public int getCountEdges() {
+    return this.countEdges;
+  }
+
+  public int[][] getAdjMatrix() {
+    return this.adjMatrix;
+  }
+
+  public String toString() {
+    String str = "";
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+        str += this.adjMatrix[i][j] + "\t";
+      }
+      str += "\n";
     }
+    return str;
+  }
 
-    public int getCountEdges() {
-        return countEdges;
+  public void addEdge(int source, int sink, int weight) {
+    if (source < 0 || source > this.countNodes - 1
+        || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
+      System.err.println("Invalid edge: " + source + sink + weight);
+      return;
     }
+    this.adjMatrix[source][sink] = weight;
+    this.countEdges++;
+  }
 
-    public int[][] getAdjMatrix() {
-        return adjMatrix;
+  public void addEdgeUnoriented(int u, int v, int w) {
+    if (u < 0 || u > this.countNodes - 1
+        || v < 0 || v > this.countNodes - 1 || w <= 0) {
+      System.err.println("Invalid edge: " + u + v + w);
+      return;
     }
+    this.adjMatrix[u][v] = w;
+    this.adjMatrix[v][u] = w;
+    this.countEdges += 2;
+  }
 
-    @Override
-    public String toString() {
-        String str = "";
-        for (int i = 0; i < this.adjMatrix.length; i++) {
-            for (int j = 0; j < this.adjMatrix[i].length; j++) {
-                str += this.adjMatrix[i][j] + "\t";
-            }
-            str += "\n";
+  public int degree(int node) {
+    if (node < 0 || node > this.countNodes - 1)
+      System.err.println("Invalid node: " + node);
+    int degree = 0;
+    for (int j = 0; j < this.adjMatrix[node].length; ++j) {
+      if (this.adjMatrix[node][j] != 0)
+        ++degree;
+    }
+    return degree;
+  }
+
+  public int highestDegree() {
+    int highest = 0;
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      int degreeNodeI = this.degree(i);
+      if (highest < degreeNodeI)
+        highest = degreeNodeI;
+    }
+    return highest;
+  }
+
+  public int lowestDegree() {
+    int lowest = this.countNodes;
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      int degreeNodeI = this.degree(i);
+      if (lowest > degreeNodeI)
+        lowest = degreeNodeI;
+    }
+    return lowest;
+  }
+
+  public graph complement() {
+    graph g2 = new graph(this.countNodes);
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+        if (i != j && this.adjMatrix[i][j] == 0) {
+          g2.addEdge(i, j, 1);
         }
-        return str;
+      }
     }
+    return g2;
+  }
 
-    public void addEdge(int source, int sink, int weight) {
-        if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
-            System.err.println("Invalid edge: " + source + " " + sink + " " + weight);
-            return;
-        }
-        this.adjMatrix[source][sink] = weight;
-        this.countEdges++;
+  public float density() {
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+  }
+
+  public boolean subGraph(graph g2) {
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+      return false;
+    for (int i = 0; i < g2.adjMatrix.length; ++i) {
+      for (int j = 0; j < g2.adjMatrix[i].length; ++j) {
+        if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0)
+          return false;
+      }
     }
+    return true;
+  }
 
-    public int degree(int node) {
-        // returns the degree of a node
-        if (node < 0 || node > this.countNodes - 1) {
-            System.err.println("Invalid node: " + node);
+  public ArrayList<Integer> bfs(int s) { // busca em largura
+    // initialization
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    // main loop
+    while (Q.size() > 0) {
+      int u = Q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+        if (this.adjMatrix[u][v] != 0) { // v é adjacente a u
+          if (desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
         }
-        int degree = 0;
-        for (int j = 0; j < this.adjMatrix[node].length; j++) {
-            if (this.adjMatrix[node][j] != 0) {
-                ++degree;
-            }
-        }
-
-        return degree;
+      }
     }
+    return R;
+  }
 
-    public int highestDegree() {
-        int aux = 0;// para melhorar o desempenho do codi, nao ficar repetindo o degree
-        int highest = 0;
-        for (int i = 0; i < this.adjMatrix.length; i++) {
-            aux = degree(i);
-            if (aux > highest) {
-                highest = aux;
-            }
-
+  public boolean connected() {
+    return this.bfs(0).size() == this.countNodes;
+  }
+  public boolean nonOriented(){
+    for(int i = 0; i<this.adjMatrix.length; i++){
+      for (int j = i+1; j< this.adjMatrix.length; j++){
+        if(this.adjMatrix[i][j] != this.adjMatrix[i][j]){
+          return false;
         }
-        return highest;
+
+      }
     }
-
-    public int lowDegree() {
-        int aux = 0;// para melhorar o desempenho do codi, nao ficar repetindo o degree
-        int low = this.adjMatrix.length;// tem que ser comparado com o maior tamanho, para ser comparado com o menor.
-        for (int i = 0; i < this.adjMatrix.length; i++) {
-            aux = degree(i);
-            if (aux < low) {
-                low = aux;
-            }
-
-        }
-        return low;
-    }
-
-    public graph complement() {
-        // returnes the complement of the corruent grafo
-        graph complement = new graph(this.adjMatrix.length);
-        for (int i = 0; i < adjMatrix.length; i++) {
-            for (int j = 0; j < adjMatrix.length; j++) {
-                if (adjMatrix[i][j] == 0) {
-                    complement.adjMatrix[i][j] = 1;
-                }
-                if (adjMatrix[i][j] != 0) {
-                    complement.adjMatrix[i][j] = 0;
-                }
-                if (i == j) {
-                    complement.adjMatrix[i][j] = 0;
-                }
-            }
-        }
-        return complement;
-
-    }
-
+    return true;
+  }
 }
--- main.java
+++ main.java
@@ -1,22 +1,46 @@
-public class main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
-        graph g1 = new graph(4);
-        g1.addEdge(0, 1, 3);
-        g1.addEdge(1, 0, 3);
-        g1.addEdge(0, 3, 4);
-        g1.addEdge(3, 0, 4);
-        g1.addEdge(3, 0, 4);
-        g1.addEdge(3, 4, 2); // AVISO
+class Main {
+  public static void main(String[] args) throws IOException {
+    graph g1 = new graph(10);
+    g1.addEdgeUnoriented(7, 5, 1);
+    g1.addEdgeUnoriented(7, 1, 1);
+    g1.addEdgeUnoriented(7, 2, 1);
+    g1.addEdgeUnoriented(1, 0, 1);
+    g1.addEdgeUnoriented(1, 4, 1);
+    g1.addEdgeUnoriented(2, 3, 1);
+    g1.addEdgeUnoriented(5, 6, 1);
+    g1.addEdgeUnoriented(6, 8, 1);
+    System.out.println(g1.bfs(7));
+    System.out.println(g1.connected());
+    graph g2 = new graph("graph.txt.txt");
+    System.out.println(g2);
+    System.out.println(g1.nonOriented());
+    
+    // graph g1 = new graph(4);
+    // g1.addEdge(0, 1, 1);
+    // g1.addEdge(1, 0, 1);
+    // g1.addEdge(0, 3, 1);
+    // g1.addEdge(3, 0, 1);
+    // System.out.println("=== g1 ===");
+    // System.out.println(g1);
+    // System.out.println("degree(0): " + g1.degree(0)); // 2
+    // System.out.println("degree(1): " + g1.degree(1)); // 1
+    // System.out.println("degree(2): " + g1.degree(2)); // 0
+    // System.out.println("degree(3): " + g1.degree(3)); // 1
+    // System.out.println("highestDegree: " + g1.highestDegree()); // 2
+    // System.out.println("lowestDegree: " + g1.lowestDegree()); // 0
+    // System.out.println("density: " + g1.density());
+    // System.out.println("\n=== g1 complement ===");
+    // System.out.println(g1.complement());
+    // graph g2 = new graph(3);
+    // g2.addEdge(0, 1, 1);
+    // g2.addEdge(1, 0, 1);
+    // System.out.println("g2 is subGraph? " + g1.subGraph(g2)); // true
+    // graph g3 = new graph(4);
+    // g3.addEdge(1, 3, 1);
+    // g3.addEdge(3, 1, 1);
+    // System.out.println("g3 is subGraph? " + g1.subGraph(g3)); // false
 
-        System.out.println(g1);
-        g1.degree(0);
-        g1.degree(1);
-        g1.degree(2);
-        g1.degree(3);
-        System.out.println("Maior grau:\n \n " + g1.highestDegree());
-        System.out.println("Menor grau:" + g1.lowDegree() + "\n");
-        System.out.println(g1);// matriz inicial
-        System.out.println("" + g1.complement());// matriz complementar
-    }
+  }
 }
